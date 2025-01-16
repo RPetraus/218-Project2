e83ad7647f34549aae34e2a1195de7483821b235
4damo5 <145879257+4damo5@users.noreply.github.com>
2025-01-15 19:46:00 -0500

    finalized code for project 1

--- main.cpp
+++ main.cpp
@@ -16,18 +16,22 @@ DigitalIn passengerSeatbelt(D5);
 DigitalIn ignitionButton(D6);
 
 DigitalOut greenIndicator(LED1);
-DigitalOut alarmLed(LED3); // change to D7 and hook up alarm later
 DigitalInOut sirenPin(PE_10);
 DigitalOut blueIndicator(LED2);
 
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
 //=====[Declaration and initialization of public global variables]=============
 
-bool alarmState2 = OFF;
+int introComplete = false; //boolean to tell whether the intro has been completed
 
 //=====[Declarations (prototypes) of public functions]=========================
 
 void inputsInit();
 void outputsInit();
+void ignitionCase();
+void driverIntroduction();
+void drivingState();
 
 //=====[Implementations of public functions]===================================
 
@@ -42,7 +46,6 @@ void inputsInit() {
 }
 
 void outputsInit() {
-  alarmLed = OFF;
   blueIndicator = OFF;
   greenIndicator = OFF;
 }
@@ -54,53 +57,73 @@ int main() {
   outputsInit();
 
   while (true) {
-    if (driverPresent) {
-      printf("\nWelcome to enhanced alarm system model 218-W24");
-    }
-    while (driverPresent) {
-      if (passengerPresent && driverSeatbelt && passengerSeatbelt) {
-        alarmState2 = ON;
-        greenIndicator = alarmState2;
-      }
-
-      else {
-        alarmState2 = OFF;
-        greenIndicator = alarmState2;
-      }
-
-      if (ignitionButton) {
-        if (passengerPresent && driverSeatbelt && passengerSeatbelt) {
-          printf("\nEngine started.");
-          greenIndicator = OFF;
-          blueIndicator = ON;
-          while (true) {
-          } // keeps the code stuck here
-        }
-
-        else {
-          printf("\nIgnition inhibited");
-          printf("\nReasons:"); // needs work to show reasons
-          sirenPin.output();                                     
-          sirenPin = LOW;
-          if (!driverPresent) {
-            printf("\nDriver not present.");
-          }
-          if (!passengerPresent) {
-            printf("\nPassenger not present.");
-          }
-          if (!driverSeatbelt) {
-            printf("\nDriver Seatbelt not fastened.");
-          }
-          if (!passengerSeatbelt) {
-            printf("\nPassenger Seatbelt not fastened.");
-          }
-          while (true) {
-          } // keeps the code stuck here
-        }
-      }
-    }
-    alarmState2 = OFF;
-    greenIndicator = alarmState2;
-    // sirenPin.input();
+    driverIntroduction();
+    drivingState();
   }
+}
+
+//=====[Implementations of public functions]===================================
+
+/*
+activates when the ignition button is pressed and acts based on a case-to-case basis
+Ex. uart messages are printed based on which buttons are not pressed to start the engine
+if all buttons are pressed the engine will start
+*/
+void ignitionCase() {
+  if (ignitionButton) {
+    if (passengerPresent && driverSeatbelt && passengerSeatbelt) {
+      uartUsb.write("\nEngine started.", 15);
+
+      greenIndicator = OFF;
+      blueIndicator = ON;
+      while (true) {} // keeps the code stuck here
+    }
+
+    else {
+      uartUsb.write("\nIgnition inhibited", 19);
+      uartUsb.write("\nReasons:", 9); 
+      sirenPin.output();
+      sirenPin = LOW;
+
+      if (!driverPresent) {
+        uartUsb.write("\nDriver not present.", 20);
+      }
+      if (!passengerPresent) {
+        uartUsb.write("\nPassenger not present.", 23);
+      }
+      if (!driverSeatbelt) {
+        uartUsb.write("\nDriver Seatbelt not fastened.", 30);
+      }
+      if (!passengerSeatbelt) {
+        uartUsb.write("\nPassenger Seatbelt not fastened.", 33);
+      }
+      while (true) {} // keeps the code stuck here
+    }
+  }
+}
+
+/*
+prints the introduction string in uart when the driver is first present
+*/
+void driverIntroduction() {
+  if (driverPresent && !introComplete) {
+    uartUsb.write("\nWelcome to enhanced alarm system model 218-W24", 46);
+    introComplete = true;
+  }
+}
+
+/*
+tells the program what state the code is in; if green led should be on/off and/or ignition pressed
+*/
+void drivingState() {
+  if (driverPresent && passengerPresent && driverSeatbelt &&
+      passengerSeatbelt) {
+    greenIndicator = ON;
+  }
+
+  else {
+    greenIndicator = OFF;
+  }
+
+  ignitionCase();
 }
